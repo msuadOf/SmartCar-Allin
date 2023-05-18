271c1f6ea4128b7d7c6a404cb71409b31c332ff3
msuad <1445617868@qq.com>
2023-05-18 10:05:56 +0800

    Update bsp_spi_mpu6500.c

--- bsp/bsp_spi_mpu6500.c
+++ bsp/bsp_spi_mpu6500.c
@@ -12,7 +12,7 @@
 
 //========SPI实现（部分函数SPI库有实现：SPI_WriteByte、SPI_ReadByte）========
 
-
+#include "STC32G_GPIO.h"
 #include "STC32G_SPI.h"
 #include "STC32G_NVIC.h"
 #include "STC32G_Switch.h"
@@ -25,17 +25,21 @@ static void SPI_config(void)
 {
     SPI_InitTypeDef SPI_InitStructure;
     SPI_InitStructure.SPI_Enable = ENABLE;        // SPI启动    ENABLE, DISABLE
-    SPI_InitStructure.SPI_SSIG = DISABLE;          // 片选位     ENABLE, DISABLE
+    SPI_InitStructure.SPI_SSIG = DISABLE;         // 片选位     ENABLE, DISABLE
     SPI_InitStructure.SPI_FirstBit = SPI_MSB;     // 移位方向   SPI_MSB, SPI_LSB
     SPI_InitStructure.SPI_Mode = SPI_Mode_Master; // 主从选择   SPI_Mode_Master, SPI_Mode_Slave
     SPI_InitStructure.SPI_CPOL = SPI_CPOL_High;   // 时钟相位   SPI_CPOL_High,   SPI_CPOL_Low
     SPI_InitStructure.SPI_CPHA = SPI_CPHA_2Edge;  // 数据边沿   SPI_CPHA_1Edge,  SPI_CPHA_2Edge
-    SPI_InitStructure.SPI_Speed = SPI_Speed_16;    // SPI速度    SPI_Speed_4, SPI_Speed_16, SPI_Speed_64, SPI_Speed_128
+    SPI_InitStructure.SPI_Speed = SPI_Speed_16;   // SPI速度    SPI_Speed_4, SPI_Speed_16, SPI_Speed_64, SPI_Speed_128
     SPI_Init(&SPI_InitStructure);
-    //NVIC_SPI_Init(ENABLE, Priority_3); // 中断使能, ENABLE/DISABLE; 优先级(低到高) Priority_0,Priority_1,Priority_2,Priority_3
+    // NVIC_SPI_Init(ENABLE, Priority_3); // 中断使能, ENABLE/DISABLE; 优先级(低到高) Priority_0,Priority_1,Priority_2,Priority_3
 
     SPI_SW(SPI_P22_P23_P24_P25); // SPI_P54_P13_P14_P15,SPI_P22_P23_P24_P25,SPI_P54_P40_P41_P43,SPI_P35_P34_P33_P32
     SPI_SS_2 = 1;
+}
+static void GPIO_config(void)
+{
+    P2_MODE_IO_PU(GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_5);
 }
 
 // void SPI_WriteByte(u8 out)
@@ -76,8 +80,6 @@ void SPI_Read_Nbytes(u8 addr, u8 *buffer, u16 size)
     SPI_CS_DISABLE(); // disable device
 }
 
-
-
 /************************************************
 写数据到spi总线上
 入口参数:
@@ -104,14 +106,18 @@ void SPI_Write_Nbytes(u8 addr, u8 *buffer, u8 size)
     SPI_CS_DISABLE(); // disable device
 }
 
-void bsp_spi_init(){
+void bsp_spi_init()
+{
     SPI_config();
+    GPIO_config();
 }
 
-void bsp_spi_read(unsigned char addr, unsigned char *buffer, int len){
-    SPI_Read_Nbytes(addr+(1<<7), buffer, len);
+void bsp_spi_read(unsigned char addr, unsigned char *buffer, int len)
+{
+    SPI_Read_Nbytes(addr + (1 << 7), buffer, len);
 }
 
-void bsp_spi_write(unsigned char addr, unsigned char *buffer, int len){
-    SPI_Write_Nbytes(addr+(0<<7), buffer, len);
+void bsp_spi_write(unsigned char addr, unsigned char *buffer, int len)
+{
+    SPI_Write_Nbytes(addr + (0 << 7), buffer, len);
 }
